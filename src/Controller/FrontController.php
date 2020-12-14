65d6be7405994df22d690db26338d1c82eeade38
David <ds@magneticlab.ch>
2020-12-14 11:37:22 +0100

    Initiated first github access

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/auth", name="auth")
+     */
+    public function auth(Request $request): Response
+    {
+        dump( $request ); die();
+    }
+}
